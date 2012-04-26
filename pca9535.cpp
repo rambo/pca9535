eced5e57617d0dacfab1e24e4ffbe1fd860813c2
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-04-26 19:31:45 +0300

    implement the basic functionality (NOTE: not actually tested, only compiled...)

--- pca9535.cpp
+++ pca9535.cpp
@@ -5,3 +5,47 @@ pca9535::pca9535()
 {
     device_address = 0x27; // Default to the address on the breakout board with no address pins pulled down (the board has pull-ups for them)
 }
+
+/**
+ * Sets the mode bits for given port (0/1), a low bit means output, default is all inputs (0xff)
+ */
+boolean pca9535::set_port_mode(byte port, byte mode)
+{
+    return this->write(0x6 + port, mode);
+}
+
+/**
+ * Sets the invert bits for given port (0/1), a high bit means inverted input, default is all normal (0x0)
+ */
+boolean pca9535::set_port_invert(byte port, byte mode)
+{
+    return this->write(0x4 + port, mode);
+}
+
+/**
+ * Reads the input bits
+ */
+boolean pca9535::read_input()
+{
+    return this->read_many(0x0, 2, this->input);
+}
+
+/**
+ * Writes the output bits
+ */
+boolean pca9535::write_output()
+{
+    return this->write_many(0x2, 2, this->output);
+}
+
+/**
+ * calls read_input and write_output, returns true if both succeed false if one fails
+ */
+boolean pca9535::sync()
+{
+    boolean ret;
+    ret = this->read_input();
+    ret = ret & this->write_output();
+    return ret;
+}
+
--- pca9535.h
+++ pca9535.h
@@ -9,7 +9,15 @@ class pca9535 : public i2c_device
 {
     public:
         pca9535(); // We need this so we can set default address
+        boolean set_port_mode(byte port, byte mode); // Sets the input/ouput bits on portA/portB
+        boolean set_port_invert(byte port, byte mode); // Sets the input inversion bits on portA/portB
+        boolean sync(); // Shortcut to read_input() and write_output()
+        boolean read_input(); // Updates the local input data from device 
+        boolean write_output(); // Updates the local output data to device
 
+        // TODO: We might be able to live with only one array for both i/o but then the sync method is useless (or at least it needs to write first and read then)
+        byte input[2]; // registers 0 and 1
+        byte output[2]; // registers 2 and 3
 };
 
 
