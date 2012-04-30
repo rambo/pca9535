aca60c10f566530118b2707cd1020a07fa8a4ce7
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-04-30 20:33:27 +0300

    nicer approach to the floating pins problem

--- examples/pca9535int/pca9535int.ino
+++ examples/pca9535int/pca9535int.ino
@@ -4,9 +4,8 @@
  * The INT pin is open collector and in case you're not using the board in this repo (which contains pull-up for the pin)
  * you will need to enable the Arduino internal pull-up (or provide external one), this sketch enables the Arduino pull-up as well
  *
- * NOTE: you must have something actively driving the input pins on PCA9535 or the floating pins will fluctuate generating so many interrupts
- * that all your cycles are spent handling them istead of doing usefull work. 
- * 
+ * Make sure you have no floating pins (or there will be interrupts coming in faster than you can handle them), one way is to define all pins
+ * as outputs first and then reconfigure to input those that are connected to switches or something.
  */
 // Get this from https://github.com/rambo/I2C
 #include <I2C.h> // For some weird reason including this in the relevant .h file does not work
@@ -45,6 +44,12 @@ void setup()
 
     // Scan the bus (do this if you can't find your device address)
     //I2c.scan();
+    
+    // Set all pins as outputs to avoid floating pins
+    expander.set_port_mode(0, 0x0);
+    expander.set_port_mode(1, 0x0);
+    // And portA bit 7 as input
+    expander.pinMode(7, INPUT);
 
     Serial.println("=== Dump ===");
     expander.dump_registers(0x0, 0x07);
@@ -87,9 +92,11 @@ void loop()
         Serial.print("expander.data[1]=0x");
         Serial.println(expander.data[1], HEX);
         read_expander = false;
+        /*
         Serial.println("=== Dump ===");
         expander.dump_registers(0x0, 0x07);
         Serial.println("=== Done ===");
+        */
     }
 }
 
