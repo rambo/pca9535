86c1fbb388f2c96df8bce2adae9380575ba00408
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-04-30 19:37:39 +0300

    tested the interrupt handling, added some comments and usefull status reporting

--- examples/pca9535int/pca9535int.ino
+++ examples/pca9535int/pca9535int.ino
@@ -1,3 +1,13 @@
+/**
+ * Example sketch demoing the the use of the interrupt on the PCA9535 chip
+ *
+ * The INT pin is open collector and in case you're not using the board in this repo (which contains pull-up for the pin)
+ * you will need to enable the Arduino internal pull-up (or provide external one), this sketch enables the Arduino pull-up as well
+ *
+ * NOTE: you must have something actively driving the input pins on PCA9535 or the floating pins will fluctuate generating so many interrupts
+ * that all your cycles are spent handling them istead of doing usefull work. 
+ * 
+ */
 // Get this from https://github.com/rambo/I2C
 #include <I2C.h> // For some weird reason including this in the relevant .h file does not work
 #include <i2c_device.h> // For some weird reason including this in the relevant .h file does not work
@@ -5,11 +15,11 @@
 // Get this from https://github.com/rambo/PinChangeInt_userData (or use the normal PinChangeInt from http://www.arduino.cc/playground/Main/PinChangeInt)
 #include <PinChangeInt_userData.h>
 #define I2C_DEVICE_DEBUG
-// Container for the device
+// Container for the device (the default constructor will take care of the device address when using the board found in this repo)
 pca9535 expander;
 
 
-#define INT_PIN 7
+#define INT_PIN A0
 
 // The variable we check in mainloop (doing I2C operations inside the interrupt will not work)
 volatile boolean read_expander;
@@ -19,31 +29,22 @@ void expander_int_received()
  */
 void expander_int_received(void* userData)
 {
-    Serial.println("expander_int_received");
+    // Enabling this may cause your device to end up in interrupt limbo (as UART uses interrupts too and re-entrant interrupts are dangerous)
+    //Serial.println("expander_int_received");
     read_expander = true;
 }
 
 void setup()
 {
     Serial.begin(115200);
-    /*
-    Serial.print("INPUT=B");
-    Serial.println(INPUT, BIN);
-    Serial.print("OUTPUT=B");
-    Serial.println(OUTPUT, BIN);
-    Serial.print("(byte)_BV(3)=B");
-    Serial.println((byte)_BV(3), BIN);
-    Serial.print("(byte)~_BV(3)=B");
-    Serial.println((byte)~_BV(3), BIN);
-    */
 
     // Initialize I2C library manually
     I2c.begin();
     I2c.timeOut(500);
     I2c.pullup(true);
 
-    // Scan the bus
-    I2c.scan();
+    // Scan the bus (do this if you can't find your device address)
+    //I2c.scan();
 
     Serial.println("=== Dump ===");
     expander.dump_registers(0x0, 0x07);
@@ -67,9 +68,14 @@ void setup()
     Serial.println("Booted");
 }
 
-
+unsigned long last_millis;
 void loop()
 {
+    if ((millis() - last_millis) > 1000)
+    {
+        Serial.println("Still alive");
+        last_millis = millis();
+    }
     if (read_expander)
     {
         expander.read_data();
